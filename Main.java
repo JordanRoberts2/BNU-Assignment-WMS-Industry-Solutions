959aeaa09e4c767ffa786a3351977d9ba52056af
Jordan Roberts <jordan.roberts2@sky.uk>
2025-05-20 13:09:17 +0100

    Added draft of restock product function

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ class Main {
         int choice;
         do {
             System.out.println("--------------//");
-            System.out.println("Welcome to the Warehouse Management System");
+            System.out.println("Welcome to the Warehouse Management System!");
             System.out.println("1. Add Product");
             System.out.println("2. Restock Product");
             System.out.println("3. View Inventory");
@@ -18,11 +18,13 @@ class Main {
 
             switch (choice) {
                 case 1 -> addProduct(scanner);
-                //case 2 -> restockProduct(scanner);
+                case 2 -> restockProduct(scanner);
                 //case 3 -> inventoryManager.printInventory(scanner);
             }
         } while (choice != 0);
         System.out.println("Exiting the system. Goodbye!");
+        System.out.println("--------------//");
+
     }
 
     private static void addProduct(Scanner scanner) {
@@ -37,5 +39,14 @@ class Main {
 
         Product product = new Product(name, id, price, stockLevel);
         System.out.println("Product added: " + product.getName());
+    }
+
+    private static void restockProduct(Scanner scanner) {
+        //Some kind of loop checking that there is at least 1 existing product to return in a list, otherwise, show error message saying 'sorry, there are no products in the system. Please add a product first'
+        System.out.print("Which product do you want to restock?:");
+        String name = scanner.nextLine();
+        System.out.print("How many items do you want to add to...");
+        int addProduct = scanner.nextInt();
+
     }
 }
